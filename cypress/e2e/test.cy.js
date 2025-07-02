ffbfe526dd1181074f7720043617a570160bd7f1
Ablaye Thiam <ablayethiam87@hotmail.com>
2025-07-02 08:09:49 -0400

    deleted sample file in fixture

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -14,28 +14,3 @@ describe("Filter Language to English ", () => {
   });
 });
 
-// describe("API Mocking in Cypress using cy.intercept Method ", () => {
-//   beforeEach(() => {
-//     cy.visit("https://demo.realworld.io/");
-//     cy.intercept("GET", "https://api.realworld.io/api/tags", {
-//       fixture: "mockTags.json",
-//     });
-//     cy.intercept(
-//       "GET",
-//       "https://api.realworld.io/api/articles?limit=10&offset=0",
-//       { fixture: "mockArticles.json" }
-//     );
-//   });
-
-//   it("Mock API Tags, and then validate on UI", () => {
-//     cy.get(".tag-list", { timeout: 1000 })
-//       .should("contain", "Cypress")
-//       .and("contain", "Playwright");
-//   });
-
-//   it("Mock the Article feed, and then validate on UI", () => {
-//     cy.get("app-favorite-button.pull-xs-right").contains("10");
-//     cy.get(".author").contains("Kailash Pathak");
-//     cy.get(".preview-link > p").contains("SLASSCOM QUALITY SUMMIT 2023");
-//   });
-// });
--- cypress/fixtures/locators.js
+++
@@ -1,3 +0,0 @@
-const LOCATORS = {
-    imputField: "textarea[aria-label",
-}
